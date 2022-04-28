75eab423772ad8604a4a013f8d61b426828ad3dd
wesley.costa <wesley.costa@opah.com.br>
2022-04-28 07:28:23 -0300

    button as link

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -57,4 +57,20 @@ describe('<Button />', () => {
     expect(screen.getByText(/Buy now/i)).toBeInTheDocument()
     expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
+
+  it('should render Button as a link', () => {
+    const { debug, container } = renderWithTheme(
+      <Button as="a" href="/link">
+        Buy now
+      </Button>
+    )
+
+    //serve para imprimir o component renderizado
+    debug(container)
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toHaveAttribute(
+      'href',
+      '/link'
+    )
+  })
 })
