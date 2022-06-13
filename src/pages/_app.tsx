51c6ebdb880fd4d323fb85d7bc395e5d4ba8819d
wesley.costa <wesley.costa@opah.com.br>
2022-06-13 19:17:30 -0300

    add script for check ts sintax and find bugs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,7 @@ function App({ Component, pageProps }: AppProps) {
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
-      <GlobalStyles />
+      <GlobalStyles removeBg />
       <Component {...pageProps} />
     </ThemeProvider>
   )
