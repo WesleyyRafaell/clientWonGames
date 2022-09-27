4860fc98902047bfd939da823f63ad8306ca98d7
Wesley Rafael <rafaelsanto10s@gmail.com>
2022-09-27 20:30:08 -0300

    create page game

---
+++ src/templates/Game/index.tsx
@@ -0,0 +1,62 @@
+import Base from 'templates/Base'
+
+import GameInfo, { GameInfoProps } from 'components/Gameinfo'
+import Gallery, { GalleryImageProps } from 'components/Gallery'
+import GameDetails, { GameDetailsProps } from 'components/GameDetails'
+
+import * as S from './styles'
+import TextContent from 'components/TextContent'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+// import Showcase from 'components/Showcase'
+
+export type GameTemplateProps = {
+  cover: string
+  gameInfo: GameInfoProps
+  gallery?: GalleryImageProps[]
+  description: string
+  details: GameDetailsProps
+  upcommingGames: GameCardProps[]
+  upcommingHighlight: HighlightProps
+  recommendedGames: GameCardProps[]
+}
+
+const Game = ({
+  cover,
+  gameInfo,
+  gallery,
+  description,
+  details
+}: GameTemplateProps) => (
+  <Base>
+    <S.Cover src={cover} role="image" aria-label="cover" />
+
+    <S.Main>
+      <S.SectionGameInfo>
+        <GameInfo {...gameInfo} />
+      </S.SectionGameInfo>
+
+      <S.SectionGallery>
+        {!!gallery && <Gallery items={gallery} />}
+      </S.SectionGallery>
+
+      <S.SectionDescription>
+        <TextContent title="Description" content={description} />
+      </S.SectionDescription>
+
+      <S.SectionGameDetails>
+        <GameDetails {...details} />
+      </S.SectionGameDetails>
+
+      {/* <Showcase
+        title="Upcomming"
+        games={upcommingGames}
+        highlight={upcommingHighlight}
+      />
+
+      <Showcase title="You may like these games" games={recommendedGames} /> */}
+    </S.Main>
+  </Base>
+)
+
+export default Game
---
+++ src/templates/Game/test.tsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { screen } from '@testing-library/react'
+
+// import Game from '.'
+
+describe('<Game />', () => {
+  it('should render the heading', () => {
+    // const { container } = render(<Game />)
+    expect(1).toBe(1)
+  })
+})
