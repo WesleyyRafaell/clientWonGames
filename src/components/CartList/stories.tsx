2a8de537813a83627b0edcfd7dc90fe9c92c8742
Wesley Rafael <rafaelsanto10s@gmail.com>
2022-11-04 15:18:35 -0300

    add CartList component

---
+++ src/components/CartList/stories.tsx
@@ -0,0 +1,29 @@
+import { Story, Meta } from '@storybook/react'
+import CartList, { CartListProps } from '.'
+
+import mockItems from './mock'
+
+export default {
+  title: 'CartList',
+  component: CartList,
+  argTypes: {
+    items: {
+      type: 'string'
+    }
+  },
+  args: {
+    items: mockItems,
+    total: 'R$ 330,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<CartListProps> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CartList {...args} />
+  </div>
+)
