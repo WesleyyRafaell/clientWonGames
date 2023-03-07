abcd993d583f07e29a7c0e8d0b34b1d9d2b6f49d
wesley costa <wesley.costa@mblabs.com.br>
2023-03-07 10:08:37 -0300

    feat: create profile me page

---
+++ src/templates/Profile/test.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Profile from '.'
+
+describe('<Profile />', () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  it('should render the heading', () => {})
+})
