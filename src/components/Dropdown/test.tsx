34c5ddd3cb3ca51178e1587106c762357ac29a0e
Wesley <rafaelsanto10s@gmail.com>
2023-10-19 10:47:40 -0300

    feat: create dropdown component

---
+++ src/components/Dropdown/test.tsx
@@ -0,0 +1,32 @@
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
+
+import Dropdown from '.'
+
+describe('<Dropdown />', () => {
+  beforeEach(() => {
+    const title = <h1 aria-label="toogle dropdown">Click here</h1>
+
+    renderWithTheme(
+      <Dropdown title={title}>
+        <span>content</span>
+      </Dropdown>
+    )
+  })
+
+  it('should render the heading', () => {
+    expect(screen.getByText('Click here')).toBeInTheDocument()
+  })
+
+  it('should handle open/close dropdown', () => {
+    const content = screen.getByText(/content/).parentElement!
+
+    expect(content).toHaveStyle({ opacity: 0 })
+    expect(content.getAttribute('aria-hidden')).toBe('true')
+
+    fireEvent.click(screen.getByLabelText(/toogle dropdown/))
+
+    expect(content).toHaveStyle({ opacity: 1 })
+    expect(content.getAttribute('aria-hidden')).toBe('false')
+  })
+})
