c6e16c9ad02f7f741c743345af067eac8bacd471
Wesley <rafaelsanto10s@gmail.com>
2024-01-08 21:31:03 -0300

    feat: populate game page

--- src/graphql/queries/games.ts
+++ src/graphql/queries/games.ts
@@ -27,3 +27,62 @@ export const QUERY_GAMES = gql`
     }
   }
 `
+export const QUERY_GAMES_BY_SLUG = gql`
+  query QueryGameBySlug($slug: String!) {
+    games(filters: { slug: { eq: $slug } }) {
+      data {
+        attributes {
+          name
+          short_description
+          description
+          price
+          rating
+          release_date
+          gallery {
+            data {
+              attributes {
+                src: url
+                label: alternativeText
+              }
+            }
+          }
+          cover {
+            data {
+              attributes {
+                src: url
+              }
+            }
+          }
+          developers {
+            data {
+              attributes {
+                name
+              }
+            }
+          }
+          publisher {
+            data {
+              attributes {
+                name
+              }
+            }
+          }
+          categories {
+            data {
+              attributes {
+                name
+              }
+            }
+          }
+          platforms {
+            data {
+              attributes {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
