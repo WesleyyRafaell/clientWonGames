68e0f281881d68531aba9b4061acc88ff7708ee8
Wesley <rafaelsanto10s@gmail.com>
2024-01-17 21:07:11 -0300

    feat: add banner in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,35 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
 
-import bannersMock from '../components/BannerSlide/mock'
 import gameCardSlider from '../components/GameCardSlider/mock'
 import highlightMock from '../components/Highlight/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 60,
+      banners: data.banners?.data.map((banner) => ({
+        img: `http://localhost:1337${banner.attributes?.image.data?.attributes?.url}`,
+        title: banner.attributes?.title,
+        subtitle: banner.attributes?.subtitle,
+        buttonLabel: banner.attributes?.button?.label,
+        buttonLink: `game/${banner.attributes?.button?.link}`,
+        ...(banner.attributes?.ribbon && {
+          ribbon: banner.attributes?.ribbon.text,
+          ribbonColor: banner.attributes?.ribbon.color,
+          ribbonSize: banner.attributes?.ribbon.size
+        })
+      })),
       newGamesSlider: gameCardSlider,
       mostPopularHighLight: highlightMock,
       mostPopularSlider: gameCardSlider,
